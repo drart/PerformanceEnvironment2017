6c0011f621c2e0928d2ed144090368ba70c0df93
Tindale <atindale@iMac-10.local>
2017-07-07 14:54:28 -0400

    added nexusui

---
+++ nexussetup.js
@@ -0,0 +1,10 @@
+nx.onload = function(){
+
+	if(!nx.isTouchDevice){
+            tilt1.canvas.hidden = true;
+	};
+
+	// set this up if myos?	
+	multislider1.setNumberOfSliders(3);
+	multislider1.canvas.hidden = true;
+};
