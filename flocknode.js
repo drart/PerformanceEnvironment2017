540d08fa09eae5fe4dd94e6fbaed257e97920c2a
Adam Tindale <adam@adamtindale.com>
2017-10-07 15:38:04 -0400

    starting move to osc interfaces

--- flocknode.js
+++ flocknode.js
@@ -9,32 +9,33 @@ flock.init({
 
 
 var s = flock.synth({
-        synthDef: {
-                              ugen: "flock.ugen.sin",
-            freq: {
-                            ugen: "flock.ugen.lfNoise",
-                freq: 1,
-                mul: 180,
-                add: 180
-            },
-            mul: {
-                                 ugen: "flock.ugen.envGen",
-                envelope: {
-                                    type: "flock.envelope.sin",
-                    duration: 0.5
-                },
-                gate: {
-                                          ugen: "flock.ugen.lfPulse",
-                    width: 0.5,
-                    freq: 1
-                }
-        }
-    }
+    synthDef: {
+                  ugen: "flock.ugen.sin",
+    expand: 6,
+    freq: {
+        ugen: "flock.ugen.lfNoise",
+    freq: 1,
+    mul: 180,
+    add: 180
+    },
+    mul: {
+             ugen: "flock.ugen.envGen",
+    envelope: {
+        type: "flock.envelope.sin",
+    duration: 0.5
+    },
+    gate: {
+              ugen: "flock.ugen.lfPulse",
+    width: 0.5,
+    freq: 1
+          }
+         }
+              }
 });
 
 s.play();
 
 var replServer = repl.start({
-      prompt: "my-app > ",
+    prompt: "my-app > ",
 });
 replServer.context.flock = flock;
