cb5b970d4f728b7c0f5e266fcecd4e3c3edf47d3
Adam Tindale <adam@adamtindale.com>
2017-10-23 23:02:59 -0400

    file cleanup

--- adam.js
+++ adam.js
@@ -105,6 +105,30 @@
         }
     });
 
+    fluid.defaults("adam.floaty", {
+        gradeNames: "flock.synth", 
+            ugen: "flock.ugen.sin",
+            expand: 6,
+            freq: {
+                ugen: "flock.ugen.lfNoise",
+                freq: 1,
+                mul: 180,
+                add: 180
+            },
+            mul: {
+                ugen: "flock.ugen.envGen",
+                envelope: {
+                type: "flock.envelope.sin",
+                duration: 0.5
+            },
+            gate: {
+                ugen: "flock.ugen.lfPulse",
+                width: 0.5,
+                freq: 1
+            }
+        }
+    });
+
     fluid.defaults("adam.cloosh", {
         gradeNames: "flock.band", 
         synthDef: {
---
+++ test/gamepads.js
@@ -0,0 +1,8 @@
+// https://github.com/360Controller/360Controller
+var pads = navigator.getGamepads();
+var controller = pads[0];
+if (controller !== null){
+	console.log(controller);
+}else {
+	//console.log("no gamepad controllers found");
+}
